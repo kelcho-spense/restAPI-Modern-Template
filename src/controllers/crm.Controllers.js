78daddf91ca10fe9451302d62d3ac81332571917
kelcho spense <kelchspense88@gmail.com>
2023-05-21 00:04:38 +0300

    first commit

--- src/controllers/crm.Controllers.js
+++ src/controllers/crm.Controllers.js
@@ -1,24 +1,25 @@
 import mongoose from "mongoose";
-import { ContactSchema } from "../models/crm.Models";
-const Contact = mongoose.model("Contact", ContactSchema);
+import { UniversitySchema } from "../models/crm.Models";
+const University = mongoose.model("University", UniversitySchema);
 
-export const addNewContact = (req, res) => {
-    let newContact = new Contact(req.body);
-    newContact.save((err, contact) => {
+export const addUniversity = (req, res) => {
+    let newUniversity = new University(req.body);
+    newUniversity.save((err, contact) => {
         err && res.send(err);
         res.json(contact);
     });
 };
-export const getContacts = (req, res) => {
-    Contact.find({}, (err, contact) => {
+export const getUniversities = (req, res) => {
+    University.find({}, (err, Uni) => {
         err && res.send(err);
-        res.json(contact);
+        res.json(Uni);
     })
 }
+// show me code for searchUniversityViaCountry
 
-export const getContactWithId = (req, res) => {
-    Contact.findById(req.params.contactId, (err, contact) => {
+export const searchUniversityViaCountry = (req, res) => {
+    University.find({ country: req.params.country }, (err, universities) => {
         err && res.send(err);
-        res.json(contact);
+        res.json(universities);
     })
 }
